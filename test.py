b8002b28ba56653c8f8ca76fcf36ae42ba43d3eb
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2025-03-10 11:17:52 -0600

    Create Reverse Method

---
+++ test.py
@@ -0,0 +1,25 @@
+'''
+Test  of reverse method for LL. 
+'''
+from linked_list import Node, LinkedList
+from reverse import reverse_with_stack
+
+node_a = Node("A")
+node_b = Node("B")
+node_c = Node("C")
+node_d = Node("D")
+
+
+ll = LinkedList()
+print(ll)
+
+#add nodes to ll
+ll.insert_at_end(node_a)
+ll.insert_at_end(node_b)
+ll.insert_at_end(node_c)
+ll.insert_at_end(node_d)
+print(ll)
+
+#test reverse method 
+ll.reverse()
+print(ll)
